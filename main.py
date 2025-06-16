a9854cd7d8c5a9f6447b6f814ec3df3160e4e355
A S M Tauhid Rahman <tauhidrahman321@gmail.com>
2025-06-16 12:55:25 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+from src.fetch_data import fetch_last_90_days
+from src.process_data import clean_data
+from src.predict_weather import forecast
+
+def run():
+    print("Fetching data...")
+    fetch_last_90_days()
+
+    print("Processing data...")
+    clean_data()
+
+    print("Forecasting next 7 days (temperature)...")
+    pred = forecast()
+    print(pred)
+
+if __name__ == "__main__":
+    run()
